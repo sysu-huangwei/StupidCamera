b9b4a0eecfb55f31cc6dba5354200491b9578543
sysu_huangwei <345911572@qq.com>
2021-04-12 16:30:30 +0800

    提交最原始的相机权限和逻辑

--- StupidCamera/StupidCamera/ViewController.m
+++ StupidCamera/StupidCamera/ViewController.m
@@ -6,9 +6,11 @@
 //
 
 #import "ViewController.h"
+#import <GPUImage/GPUImage.h>
 
 @interface ViewController ()
-
+@property (strong, nonatomic) GPUImageStillCamera *camera;
+@property (strong, nonatomic) GPUImageView *imageView;
 @end
 
 @implementation ViewController
@@ -16,6 +18,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    _camera = [[GPUImageStillCamera alloc] initWithSessionPreset:AVCaptureSessionPresetPhoto cameraPosition:AVCaptureDevicePositionFront];
+    _camera.outputImageOrientation = UIInterfaceOrientationPortrait;//设置照片的方向为设备的定向
+    _camera.horizontallyMirrorFrontFacingCamera = YES;//设置前置是否为镜像
+    [_camera setCaptureSessionPreset:AVCaptureSessionPresetPhoto];
+    _imageView = [[GPUImageView alloc] initWithFrame:self.view.bounds];
+    [_camera addTarget:_imageView];
+    [self.view addSubview:_imageView];
+    [_camera startCameraCapture];
 }
 
 
