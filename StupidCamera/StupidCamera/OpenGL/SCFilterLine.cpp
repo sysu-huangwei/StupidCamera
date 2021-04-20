6e757ac15bbf8b8e12b0f291a2ff39a823cd716f
sysu_huangwei <345911572@qq.com>
2021-04-20 19:47:56 +0800

    提交画线的filter

---
+++ StupidCamera/StupidCamera/OpenGL/SCFilterLine.cpp
@@ -0,0 +1,103 @@
+//
+//  SCFilterLine.cpp
+//
+//  Created by rayyy on 2021/4/20.
+//
+
+#include "SCFilterLine.hpp"
+#include <string.h>
+
+const char *kSCFilterLineVertexShaderString = SHADER_STRING_CPP
+(
+ attribute vec2 a_position;
+ 
+ void main()
+ {
+    gl_PointSize = 5.0;
+    gl_Position = vec4(a_position * 2.0 - 1.0, 0.0, 1.0);
+ }
+);
+
+const char *kSCFilterLineFragmentShaderString = SHADER_STRING_CPP
+(
+ precision highp float;
+ 
+ void main()
+ {
+    gl_FragColor = vec4(1,0,0,1);
+ }
+);
+
+SCFilterLine::SCFilterLine():SCFilterBase() {
+    
+}
+
+SCFilterLine::~SCFilterLine() {
+    if (this->lines) {
+        delete [] this->lines;
+        this->lines = nullptr;
+    }
+}
+
+void SCFilterLine::init() {
+    SCFilterBase::initWithVertexStringAndFragmentString(kSCFilterLineVertexShaderString, kSCFilterLineFragmentShaderString);
+    positionAttribute = glGetAttribLocation(programID, "a_position");
+//    textureCoordinateAttribute = glGetAttribLocation(programID, "a_texCoord");
+//    inputImageTextureUniform = glGetUniformLocation(programID, "u_texture");
+}
+
+unsigned SCFilterLine::render() {
+    if (!this->lines) {
+        return isRenderToOutside ? textureIDOutside : srcTextureID;
+    }
+    
+    if (isRenderToOutside) {
+        glBindFramebuffer(GL_FRAMEBUFFER, fboIDOutside);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureIDOutside, 0);
+    } else {
+        glBindFramebuffer(GL_FRAMEBUFFER, fboID);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureID, 0);
+    }
+    
+    glViewport(0, 0, width, height);
+    
+//    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//    glClearColor(0.0f,0.0f,0.0f,1.0f);
+    
+    glUseProgram(programID);
+    
+    glEnableVertexAttribArray(positionAttribute);
+    glVertexAttribPointer(positionAttribute, 2, GL_FLOAT, false, 0, lines);
+    
+//    glEnableVertexAttribArray(textureCoordinateAttribute);
+//    glVertexAttribPointer(textureCoordinateAttribute, 2, GL_FLOAT, false, 0, textureCoordinates);
+    
+//    glActiveTexture(GL_TEXTURE2);
+//    glBindTexture(GL_TEXTURE_2D, srcTextureID);
+//    glUniform1i(inputImageTextureUniform, 2);
+    
+    glLineWidth(5.0f);
+    glDrawArrays(GL_LINES, 0, linesCount);
+    
+    afterDraw();
+    return SCFilterBase::render();
+}
+
+void SCFilterLine::setSrcTextureID(unsigned srcTextureID) {
+    this->srcTextureID = srcTextureID;
+}
+
+
+void SCFilterLine::setLines(float *lines, int linesCount) {
+    if (linesCount != this->linesCount) {
+        if (this->lines) {
+            delete [] this->lines;
+            this->lines = nullptr;
+        }
+        this->linesCount = linesCount;
+        this->lines = new float[linesCount * 4];
+    }
+    if (lines && linesCount > 0) {
+        memcpy(this->lines, lines, sizeof(float) * linesCount * 4);
+    }
+}
---
+++ StupidCamera/StupidCamera/OpenGL/SCFilterLine.hpp
@@ -0,0 +1,41 @@
+//
+//  SCFilterLine.hpp
+//
+//  Created by rayyy on 2021/4/20.
+//
+
+#ifndef SCFilterLine_hpp
+#define SCFilterLine_hpp
+
+#include "SCFilterBase.hpp"
+
+/// 画线的滤镜
+class SCFilterLine : public SCFilterBase {
+public:
+    SCFilterLine();
+    ~SCFilterLine();
+    
+    /// 初始化，必须在GL线程，子类实现这个方法去做GL相关的初始化操作
+    virtual void init() override;
+    
+    /// 渲染，必须在GL线程
+    /// @return 结果纹理ID
+    virtual unsigned render() override;
+    
+    /// 设置输入图像的纹理ID
+    /// @param srcTextureID 输入图像的纹理ID
+    void setSrcTextureID(unsigned srcTextureID);
+    
+    /// 设置需要画的线，内部会做拷贝【此接口和render并行调用会有线程问题，需要保证先后顺序】
+    /// @param lines 线信息，每个线2个顶点(4个float) {line1.A.x, line1.A.y, line1.B.x, line1.B.y, line2.A.x, line2.A.y, line2.B.x……}  需要归一化到0到1的点
+    /// @param linesCount 线的数量，一般是points的长度/4
+    void setLines(float *lines, int linesCount);
+    
+private:
+    int inputImageTextureUniform = -1;
+    unsigned srcTextureID = 0;
+    float *lines = nullptr;
+    int linesCount = 0;
+};
+
+#endif /* SCFilterLine_hpp */
