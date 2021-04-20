3e80d6981a47fdbdc270efa186713ffe75f55ee7
sysu_huangwei <345911572@qq.com>
2021-04-20 19:37:40 +0800

    shader内部把点从0~1变成 -1~1

--- StupidCamera/StupidCamera/OpenGL/SCFilterPoint.cpp
+++ StupidCamera/StupidCamera/OpenGL/SCFilterPoint.cpp
@@ -15,7 +15,7 @@ const char *kSCFilterPointVertexShaderString = SHADER_STRING_CPP
  void main()
  {
     gl_PointSize = 5.0;
-    gl_Position = vec4(a_position,0.0,1.0);
+    gl_Position = vec4(a_position * 2.0 - 1.0, 0.0, 1.0);
  }
 );
 
