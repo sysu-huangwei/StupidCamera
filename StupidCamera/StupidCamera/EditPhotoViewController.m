a78b4041878171ee9c74b8422a16fd5b3b1b5ff1
sysu_huangwei <345911572@qq.com>
2021-06-07 15:34:28 +0800

    添加保存按钮

--- StupidCamera/StupidCamera/EditPhotoViewController.m
+++ StupidCamera/StupidCamera/EditPhotoViewController.m
@@ -27,12 +27,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self initSaveButton];
     _originPicture = [[GPUImagePicture alloc] initWithImage:_originImage];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
+    [self initSaveButton];
     GPUImageFilter *filter = [[GPUImageFilter alloc] init];
     [_originPicture addTarget:filter];
     [filter addTarget:self.imageView];
@@ -46,18 +46,20 @@
         _saveButton = [[UIButton alloc] init];
         [self.bottomView addSubview:_saveButton];
         [_saveButton setTitle:@"保存" forState:UIControlStateNormal];
-        [_saveButton setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
-        [_saveButton setTitleColor:UIColor.greenColor forState:UIControlStateHighlighted];
+        UIImage *image = [UIImage imageNamed:@"confirm"];
+        [_saveButton setImage:image forState:UIControlStateNormal];
     }
-    _saveButton.backgroundColor = UIColor.systemPinkColor;
+    CGFloat saveWidthHeight = 80;
+    CGFloat toTop = (self.bottomView.bounds.size.height - saveWidthHeight) * 0.5;
+    CGFloat toLeft = (self.bottomView.bounds.size.width - saveWidthHeight) * 0.5;
     _saveButton.translatesAutoresizingMaskIntoConstraints = NO;
-    NSLayoutConstraint *ConstraintTop = [NSLayoutConstraint constraintWithItem:_saveButton attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.bottomView attribute:NSLayoutAttributeTop multiplier:1.0 constant:50];
+    NSLayoutConstraint *ConstraintTop = [NSLayoutConstraint constraintWithItem:_saveButton attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.bottomView attribute:NSLayoutAttributeTop multiplier:1.0 constant:toTop];
     [self.bottomView addConstraint:ConstraintTop];
-    NSLayoutConstraint *ConstraintLeft = [NSLayoutConstraint constraintWithItem:_saveButton attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.bottomView attribute:NSLayoutAttributeLeft multiplier:1.0 constant:100];
+    NSLayoutConstraint *ConstraintLeft = [NSLayoutConstraint constraintWithItem:_saveButton attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.bottomView attribute:NSLayoutAttributeLeft multiplier:1.0 constant:toLeft];
     [self.bottomView addConstraint:ConstraintLeft];
-    NSLayoutConstraint *ConstraintRight = [NSLayoutConstraint constraintWithItem:_saveButton attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.bottomView attribute:NSLayoutAttributeRight multiplier:1.0 constant:-100];
+    NSLayoutConstraint *ConstraintRight = [NSLayoutConstraint constraintWithItem:_saveButton attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.bottomView attribute:NSLayoutAttributeRight multiplier:1.0 constant:-toLeft];
     [self.bottomView addConstraint:ConstraintRight];
-    NSLayoutConstraint *ConstraintBottom = [NSLayoutConstraint constraintWithItem:_saveButton attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.bottomView attribute:NSLayoutAttributeBottom multiplier:1.0 constant:-50];
+    NSLayoutConstraint *ConstraintBottom = [NSLayoutConstraint constraintWithItem:_saveButton attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.bottomView attribute:NSLayoutAttributeBottom multiplier:1.0 constant:-toTop];
     [self.bottomView addConstraint:ConstraintBottom];
     [_saveButton addTarget:self action:@selector(saveClick:) forControlEvents:UIControlEventTouchUpInside];
 }
