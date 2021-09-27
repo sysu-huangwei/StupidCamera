5f83936cb01f9f97be1fd6c5793e6477e18436e9
sysu_huangwei <345911572@qq.com>
2021-09-27 10:45:41 +0800

    bugfix：参数传递错误

--- StupidCamera/StupidCamera/OpenGL/filters/SCFilterBlur.cpp
+++ StupidCamera/StupidCamera/OpenGL/filters/SCFilterBlur.cpp
@@ -19,11 +19,11 @@ void SCFilterBlur::resize(int width, int height) {
     FilterChain::resize(width, height);
     for (const std::shared_ptr<FilterNode> &filterNode : allFilterNodes) {
         filterNode->filter->resize(width, height);
-        if (filterNode->id == "SCFilterBlurSubH") {
+        if (filterNode->id == "blurH") {
             std::shared_ptr<SCFilterBlurSub> blurFilterH = std::static_pointer_cast<SCFilterBlurSub>(filterNode->filter);
             blurFilterH->setOffset(1.0f / (float)width, 0);
         }
-        if (filterNode->id == "SCFilterBlurSubV") {
+        if (filterNode->id == "blurV") {
             std::shared_ptr<SCFilterBlurSub> blurFilterV = std::static_pointer_cast<SCFilterBlurSub>(filterNode->filter);
             blurFilterV->setOffset(0, 1.0f / (float)height);
         }
